273d62a58bce768630eb94223814b3fd5a052345
pn <143857858+pn-80@users.noreply.github.com>
2024-12-10 21:37:51 +0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import './App.css';
 import Homepage from './FikaWelcome.js';
